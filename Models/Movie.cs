dde0792a75c9569ed466de3958d81c302eb9e494
Ильнур <ilnur.ibragimov.1993@yandex.ru>
2019-08-19 01:34:58 +0500

    Перевод ошибок

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,27 +8,29 @@ namespace MvcMovie.Models
 	{
 		public int ID { get; set; }
 
-		[StringLength(60, MinimumLength = 3)]
-		[Required]
+		[StringLength(60, MinimumLength = 3, ErrorMessage = "TitleStringLength")]
+		[Required(ErrorMessage = "Required")]
 		public string Title { get; set; }
 
 		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
+		[Required(ErrorMessage = "Required")]
 		public DateTime ReleaseDate { get; set; }
 
-		[Range(1, 100)]
+		[Range(1, 100, ErrorMessage = "PriceRange")]
 		[DataType(DataType.Currency)]
 		[Column(TypeName = "decimal(18, 2)")]
+		[Required(ErrorMessage = "Required")]
 		public decimal Price { get; set; }
 
-		[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-		[Required]
-		[StringLength(30)]
+		[RegularExpression(@"^([A-Z]+[a-zA-Z""'\s-]*)|([А-Я]+[а-яА-Я""'\s-]*)$", ErrorMessage = "Regular")]
+		[StringLength(30, ErrorMessage = "GenreStringLength")]
+		[Required(ErrorMessage = "Required")]
 		public string Genre { get; set; }
 
-		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-		[StringLength(5)]
-		[Required]
+		[RegularExpression(@"^([A-Z]+[a-zA-Z0-9""'\s-]*)|([А-Я]+[а-яА-Я0-9""'\s-]*)$", ErrorMessage = "Regular")]
+		[StringLength(5, ErrorMessage = "RatingStringLength")]
+		[Required(ErrorMessage = "Required")]
 		public string Rating { get; set; }
 	}
 }
