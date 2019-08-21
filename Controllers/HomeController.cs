6238de0b8914979a37008e9727e867eeb3efa28e
Ильнур <ilnur.ibragimov.1993@yandex.ru>
2019-08-21 17:09:00 +0500

    Changes langhuage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,31 +14,44 @@ namespace MvcMovie.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index()
+        public string GetCulture()
+        {
+            return $"CurrentCulture:{CultureInfo.CurrentCulture.Name}, CurrentUICulture:{CultureInfo.CurrentUICulture.Name}";
+        }
+        
+        public IActionResult Index()
 		{
-			return View();
+
+            var qwe = CultureInfo.CurrentCulture;
+
+
+            return View();
 		}
 
 		public IActionResult Privacy()
 		{
 			return View();
 		}
-
+		//ща почитаю
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-		[HttpPost]
-		public IActionResult SetLanguage(string culture, string returnUrl)
-		{
-			Response.Cookies.Append(
-				CookieRequestCultureProvider.DefaultCookieName,
-				CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-				new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
-			);
- 
-			return LocalRedirect(returnUrl);
-		}
-	}
+
+
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+    }
 }
